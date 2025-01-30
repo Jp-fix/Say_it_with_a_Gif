3d83229482c200fd84b802917aa599c0f5922e67
Jp-fix <jpatrick.dev@pm.me>
2025-01-30 22:19:59 +0100

    update css minimalism

--- index.js
+++ index.js
@@ -7,25 +7,23 @@ const modalContent = document.getElementById("modal-content")
 const closeModalContainer = document.getElementById("close-modal-container")
 const shareBtn = document.getElementById("share-btn")
 const downloadBtn = document.getElementById("download-btn")
+const downloadInner = document.getElementById("download-inner")
 
 gifMessageSelector.addEventListener("change", messageSelection)
 sendBtn.addEventListener("click", renderMessageGif)
 closeModalContainer.addEventListener("click", closeModal)
-downloadBtn.addEventListener("click",downloadGif)
+shareBtn.addEventListener("click",shareGif)
 
 function messageSelection(){
-
     const selectedMessage = gifMessageSelector.value
     return selectedMessage
 }
 
 function matchingMessageAndGif(){
     const messageToGif = messageSelection()
-
     const gif = gifData.filter(function(matching){
         return matching.tags.includes(messageToGif)
     })
-
   return gif
 }
 
@@ -36,19 +34,31 @@ function showModal(){
 function closeModal(){
     modalContainer.style.display="none"
 }
+
 function renderMessageGif(){
     matchingMessageAndGif()
     showModal()
     const selectedGif = matchingMessageAndGif()
-    
     modalContent.innerHTML = 
     `
         <div class="result-message-gif">
             <img src= "${selectedGif[0].url}" alt= "${selectedGif[0].alt}">
         </div> 
     ` 
+    downloadInner.innerHTML =
+    `
+            <label for="download-btn">Enregistrer</label>
+            <a href="${selectedGif[0].url}" download>
+            <button class="download-btn" id="download-btn"><img  src="logo/Download Circle Icon.svg" alt="logo-download-gif">
+            </a>
+        </button>
+    `
 }
 
-function downloadGif(){
-    `document.getElementById("download-btn").href = ${selectedGif[0].url}`
+function shareGif(){
+    const selectedGif = matchingMessageAndGif()
+    navigator.share({
+        url:selectedGif[0].url,
+        title: selectedGif[0].name
+    })
 }
